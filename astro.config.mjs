fbb9ccad478833ea0ba5e4e0134149667820dd57
Kelisei <frankestudios71@gmail.com>
2024-01-10 21:02:03 -0300

    Github workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  site: 'https://kelisei.github.io',
+  base: '/https://github.com/Kelisei/kelisei.github.io',
 });
