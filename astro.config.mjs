afc2e8c60d0e488a915715aa4d936e586890c294
Kelisei <frankestudios71@gmail.com>
2026-09-23 10:01:57 -0300

    feat: redesign portfolio with Catppuccin Mocha theme, new projects, and markdown blog

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,7 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
 
-// https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   site: 'https://kelisei.github.io',
-  base: '/https://github.com/Kelisei/kelisei.github.io',
 });
